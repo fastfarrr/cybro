092d37d2a698db080158552cf2abfeb2139e1028
salman <salmancmd665@gmail.com>
2026-07-04 16:58:14 +0530

    [ADD] Initial commit

---
+++ Discount_limit/__manifest__.py
@@ -0,0 +1,14 @@
+# # -*- coding: utf-8 -*-
+{
+    'name': 'Discount_Limit',
+    'version':"19.0.1.0",
+    'lisence':"LGPL-3",
+    'author':"faris",
+    'category':'Discount Management',
+    'summary':"an erp for discount limit to give inside sales",
+    'sequence':2,
+    'installable':True,
+    'depends':['base','sale'],
+    'data':["views/discount_limit.xml"]
+
+}
