c3d8d4aaa34c1d12959e3ed1c573a207c9787092
salman <salmancmd665@gmail.com>
2026-07-09 17:46:12 +0530

    [UPDATE] Attachment

--- attachment/models/purchase_attachment.py
+++ attachment/models/purchase_attachment.py
@@ -1,9 +1,10 @@
 # # -*- coding: utf-8 -*-
 from odoo import models,fields
 class PurchaseConfigSettings(models.TransientModel):
+    """class for purchase settings"""
     _inherit = 'res.config.settings'
 
-    is_file_attached=fields.Boolean(string="Purchase Attachment",default=False)
+    is_file_attached=fields.Boolean(string="Purchase Attachment")
 
     def set_values(self):
         super(PurchaseConfigSettings,self).set_values()
@@ -14,7 +15,7 @@ class PurchaseConfigSettings(models.TransientModel):
     def get_values(self):
         res=super(PurchaseConfigSettings,self).get_values()
         value=self.env['ir.config_parameter'].sudo().get_param(
-            'Attachment.is_file_attached'
+            'Attachment.is_file_attached',default=False
         )
-        res.update(is_file_attached=value)
+        res.update(is_file_attached= value == 'True')
         return res
--- attachment/models/purchase_order.py
+++ attachment/models/purchase_order.py
@@ -1,23 +1,80 @@
-from odoo import models
+from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 
 class PurchaseOrder(models.Model):
-    _inherit= ["purchase.order"]
+    """class for purchase order"""
+    _inherit = "purchase.order"
 
-    def button_confirm(self):
+    is_file_valid = fields.Boolean(string="Attachment",
+                                   compute="_compute_is_file_valid",
+                                   tracking=True)
+
+
+    # @api.depends('message_attachment_count')
+    # def _compute_show_warning_message(self):
+    #     """To show warning message based on settings"""
+    #     show_warning=(self.env['ir.config_parameter'].
+    #                   sudo().
+    #                   get_param('Attachment.is_file_attached') == True)
+    #
+    #     print(show_warning)
+    #
+    #     for order in self:
+    #         # order.show_warning_message=show_warning and not order.is_file_valid
+    #         if show_warning:
+    #             order.is_file_valid = False
+    #         elif:
+    #             or
+    #         else:
+    #             order.show_warning_message=True
+
+
+
+
+
+    @api.depends('message_attachment_count')
+    def _compute_is_file_valid(self):
+        """check the file valid or not depended on attachment_count"""
         for order in self:
-            attachment=self.env["ir.attachment"].search(
+            attachment=self.env['ir.attachment'].search(
                 [('res_model', '=', 'purchase.order'),
                  ('res_id', '=', order.id),
-                 ('mimetype','in',('application/pdf','image/png',
-                                   'image/jpeg','image/jpg')),
+                 ('mimetype', 'in', ('application/pdf',
+                                     'image/png',
+                                     'image/jpeg',
+                                     'image/jpg'))
                 ]
             )
-            if not attachment:
-                raise UserError("Please attach at least a file")
+
+
+            order.is_file_valid = attachment
+            print("gfh", order.is_file_valid)
+
+
+
+
+    def button_confirm(self):
+        "checking is there any attachment attached to chatter"
+        required_attachment = (self.env['ir.config_parameter']
+                               .sudo().get_param('Attachment.is_file_attached'))
+
+        if required_attachment == 'True':
+            for order in self:
+                attachment = self.env["ir.attachment"].search(
+                    [('res_model', '=', 'purchase.order'),
+                     ('res_id', '=', order.id),
+                     ('mimetype', 'in', ('application/pdf', 'image/png',
+                                         'image/jpeg', 'image/jpg')),
+                     ]
+                )
+
+                if not attachment:
+                    raise UserError("Please attach at least one image or pdf")
 
         return super().button_confirm()
+
+
 
 
 
