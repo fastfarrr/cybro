dc0746f6a6b84b349df90f79a38c719ec00d0b9a
salman <salmancmd665@gmail.com>
2026-07-13 17:42:52 +0530

    [UPDATE] VENDOR PRODUCT LIST

--- vendor_product_list/models/purchase_order.py
+++ vendor_product_list/models/purchase_order.py
@@ -1,18 +1,52 @@
-from odoo import models,fields,api
-from odoo import Command
+#-*- coding: utf-8 -*-
+from odoo import models, fields,api
+
 class PurchaseOrder(models.Model):
+    """This class inherited the model purchase.order """
     _inherit = "purchase.order"
 
-    is_vendor_product = fields.Boolean(string="Vendor Product")
+    is_vendor_product = fields.Boolean(string="Vendor Products")
+    product_ids=fields.Many2many(comodel_name='product.product',compute='_compute_product_ids')
 
-    @api.onchange('product_id','partner_id')
-    def product_id_on_change(self):
+    @api.depends('is_vendor_product','partner_id')
+    def _compute_product_ids(self):
         for order in self:
-            if order.is_vendor_product:
-                a=self.env['purchase.order.line'].Command.create({
-                    ('product_id','=',order.partner_id.partner_id.name.id)
-                })
-                order.purchase_order_line=a
+            if order.is_vendor_product and order.partner_id:
+                products=self.env['product.product'].search([
+                    ('product_tmpl_id.seller_ids.partner_id'
+                         , '='
+                         , self.partner_id.id)
+                ])
+            else:
+                products = self.env['product.product'].search([
+                                    ('purchase_ok', '=', True)
+                                ])
+            order.product_ids = products
+
+
+
+    # @api.onchange('partner_id','is_vendor_product')
+    # def _onchange_vendor_products(self):
+    #     products = self.env['product.product'].search([
+    #         ('product_tmpl_id.seller_ids.partner_id', '=', self.partner_id.id)
+    #     ])
+    #     print(products)
+    #     if self.is_vendor_product:
+    #         self.order_line = [Command.clear()]
+    #         for product in products:
+    #             self.order_line = [
+    #                 Command.create({
+    #                     'product_id': product.id,
+    #                     'product_qty':1,
+    #                     'price_subtotal':product.standard_price
+    #                 })
+    #             ]
+    #     else:
+    #         self.order_line = [Command.clear()]
+    #         self.order_line = [Command.create({
+    #             'product_id': []
+    #         })]
+
 
 
 
