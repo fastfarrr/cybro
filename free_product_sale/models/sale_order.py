7894aee1d0bc647e583259d8041e813c0a1faa48
salman <salmancmd665@gmail.com>
2026-08-07 14:49:20 +0530

    [UPDATE] Free Product Sale

--- free_product_sale/models/sale_order.py
+++ free_product_sale/models/sale_order.py
@@ -6,10 +6,9 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     '''This class is inherited  from sale order'''
     _inherit = 'sale.order'
-    free_product_id = fields.Many2one('product.product')
+    # free_product_id = fields.Many2one('product.product')
     excluded_product_ids = fields.Many2many('product.product',
                                             string="Excluded Products")
-
 
     def check_free_product(self):
         '''This function works when the button is clicked'''
@@ -17,22 +16,16 @@ class SaleOrder(models.Model):
             [('free_product', '=', True)])
 
 
-        for product in product:
-
-            if product not in self.order_line.product_id:
-                self.write({'order_line': [Command.create({
-                    'product_id': product.id,
-                    'name': product.name,
-                    'product_uom_qty': 1,
-                    'price_unit': 0,
-                    'tax_ids': [],
-                })]})
-
-
-
         if not product:
-            raise ValidationError("No product is configured")
-
-
-
-
+            raise ValidationError("Not any product is configured as free product")
+        else:
+            for product in product:
+                if product not in self.order_line.product_id:
+                    if product not in self.excluded_product_ids:
+                        self.write({'order_line': [Command.create({
+                            'product_id': product.id,
+                            'name': product.name,
+                            'product_uom_qty': 1,
+                            'price_unit': 0,
+                            'tax_ids': [],
+                        })]})
--- free_product_sale/models/sale_order_line.py
+++ free_product_sale/models/sale_order_line.py
@@ -6,27 +6,16 @@ class SaleOrderLine(models.Model):
     _description = "Sale Order Line"
 
     product_id = fields.Many2one('product.product')
-    sale_order_id = fields.Many2one('sale.order')
     free_product=fields.Boolean(related="product_id.free_product")
-    excluded_product_ids=fields.Many2many(related="sale_order_id.excluded_product_ids")
 
 
     def exclude_free_products(self):
 
         if self.free_product:
             stored_free_product=self.product_id
-            print('stored product',stored_free_product)
 
-            self.order_id = self.sale_order_id.id
-
-            a=self.write({
-                'excluded_product_ids':[(fields.Command.link(stored_free_product.id))]
-            })
-            print(a)
+            self.order_id.write({'excluded_product_ids':[(fields.Command.link(stored_free_product.id))]})
             self.unlink()
-
-        else:
-            print('got some issue while unlinking')
 
 
 
